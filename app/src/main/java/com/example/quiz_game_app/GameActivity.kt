37740150cae28374c93f2c0becc66b0934ab9c55
Divyanshu Verma <divyanshu.verma1993@gmail.com>
2023-02-08 23:23:23 -0500

    adds background music to game screen

--- app/src/main/java/com/example/quiz_game_app/GameActivity.kt
+++ app/src/main/java/com/example/quiz_game_app/GameActivity.kt
@@ -1,6 +1,7 @@
 package com.example.quiz_game_app
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -14,9 +15,19 @@ import android.widget.Button
 import com.example.quiz_game_app.databinding.ActivityMainBinding
 
 class GameActivity : AppCompatActivity() {
-
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.glory)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(mediaPlayer != null) {
+            mediaPlayer.stop()
+        }
     }
 }
